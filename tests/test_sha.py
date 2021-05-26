06dfa46ffb8756dace530369180b0170fad3eee4
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-05-27 01:51:26 +0300

    precommit

--- tests/test_sha.py
+++ tests/test_sha.py
@@ -10,5 +10,3 @@ def test_sha256():
     assert _hash == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
     assert _hash.hash == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
 
-
-test_sha256()
